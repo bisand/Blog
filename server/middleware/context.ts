642755ef14623469ac7cec2c5da53cb6f7ffdb98
André Biseth <andre@biseth.net>
2023-07-27 20:04:31 +0000

    Testing out a few things

--- server/middleware/context.ts
+++ server/middleware/context.ts
@@ -1,6 +1,8 @@
+import { KVNamespace } from "@cloudflare/workers-types";
+
 export default defineEventHandler((event) => {
-    // if (!event.context.cloudflare) {
+    if (!event.context.cloudflare) {
         console.log('New request: ' + getRequestURL(event))
-        // event.context.cloudflare = { env: {} }; // initialize the context
-    // }
+        event.context.cloudflare = { env: { IMAGES: {} as KVNamespace } }; // initialize the context
+    }
 })
