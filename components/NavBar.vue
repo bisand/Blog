1d80c67a14ddaf9d35c1b97625ce9d9dc3203d3f
André Biseth <andre@biseth.net>
2023-08-05 14:40:25 +0000

    Moved navbar to own component

---
+++ components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="bg-slate-50 dark:bg-gray-800 p-4 shadow-lg fixed">
+        <div class="container mx-auto">
+            <div class="flex items-center justify-between">
+                <NuxtLink to="/" class="text-xl font-bold">Developer Blog</NuxtLink>
+                <ul class="flex space-x-4">
+                    <li>
+                        <NuxtLink to="/" class="align-middle">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/articles" class="align-middle">Articles</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about" class="align-middle">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/contact" class="align-middle">Contact</NuxtLink>
+                    </li>
+                    <li>
+                        <ColorModePicker />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,9 @@
 <template>
-    <div
-        :class="['bg-white', darkMode ? 'dark' : 'dark:bg-gray-900', 'text-black', darkMode ? 'dark:text-white' : 'text-white', 'transition-colors duration-300']">
-
-        <!-- Navbar -->
-        <nav class="bg-blue-600 dark:bg-gray-800 p-4">
-            <div class="container mx-auto">
-                <div class="flex items-center justify-between">
-                    <NuxtLink to="/" class="text-white text-xl font-bold">Developer Blog</NuxtLink>
-                    <ul class="flex space-x-4">
-                        <li>
-                            <NuxtLink to="/" class="text-white">Home</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/articles" class="text-white">Articles</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/about" class="text-white">About</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/contact" class="text-white">Contact</NuxtLink>
-                        </li>
-                        <li>
-                            <ColorModePicker />
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <NavBar />
         <!-- Main Content -->
         <main class="container mx-auto p-4">
             <slot />
         </main>
-    </div>
 </template>
 
 <script>
