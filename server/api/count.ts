5d68c2b920f992566c60b99cd43f91171bf23a89
André Biseth <andre@biseth.net>
2023-08-14 20:32:25 +0000

    Adjusted package versions

--- server/api/count.ts
+++ server/api/count.ts
@@ -1,12 +1,12 @@
-import { Redis } from '@upstash/redis/cloudflare'
+// import { Redis } from '@upstash/redis/cloudflare'
 
-export default defineEventHandler(async (event) => {
+// export default defineEventHandler(async (event) => {
 
-    const redis = new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL as string,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
-    })
+//     const redis = new Redis({
+//         url: process.env.UPSTASH_REDIS_REST_URL as string,
+//         token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+//     })
 
-    const count = await redis.incr("counter");
-    return count;
-})
+//     const count = await redis.incr("counter");
+//     return count;
+// })
